2d75df6a4993af2c90a203143a5a95bcfacfd9c5
Konstantin Yemelianov <k.yemelianov@gmail.com>
2021-04-16 15:36:00 +0300

    week-4 added nat

---
+++ week-4/modules/ec2_instances/main.tf
@@ -0,0 +1,92 @@
+resource "aws_instance" "PublicInstance" {
+  ami           = "ami-00f9f4069d04c0c6e"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  count         = 1
+  subnet_id     = var.public_subnets_id
+
+  vpc_security_group_ids = [
+      aws_security_group.allow_ssh.id,
+      aws_security_group.allow_http.id,
+      aws_security_group.allow_PostgreSQL.id,
+      aws_security_group.allow_all_outbound.id
+  ]
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_instance" "PrivateInstance" {
+  ami           = "ami-00f9f4069d04c0c6e"
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  count         = 1
+  subnet_id     = var.private_subnets_id
+
+  vpc_security_group_ids = [
+      aws_security_group.allow_ssh.id,
+      aws_security_group.allow_http.id,
+      aws_security_group.allow_PostgreSQL.id,
+      aws_security_group.allow_all_outbound.id
+  ]
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "allow_PostgreSQL" {
+  name        = "allow_PostgreSQL"
+  description = "Allow PostgreSQL inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "PostgreSQL from VPC"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_all_outbound" {
+  name        = "allow_all_outbound"
+  description = "Allow All outbound traffic"
+  vpc_id      = var.vpc_id
+
+  egress {
+    description = "All outbound from VPC"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ week-4/modules/ec2_instances/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  description = "Region"
+}
+
+variable "key_name" {
+  description = "SSH key name"
+}
+
+variable "public_subnets_id" {}
+
+variable "private_subnets_id" {}
+
+variable "vpc_id" {}
